58d94beea05da2697c99cad278b3785fd3723f73
Aleix Pol <aleixpol@kde.org>
2014-02-10 19:47:47 +0100

    Merge kquitapp into KDBusAddons
    Make it compile properly
    
    CCMAIL: faure@kde.org

--- src/tools/kquitapp/kquitapp.cpp
+++ src/tools/kquitapp/kquitapp.cpp
@@ -24,14 +24,14 @@
 int main(int argc, char* argv[])
 {
     QCoreApplication app(argc, argv);
-    app.setApplicationName("kquitapp");
-    app.setApplicationVersion("2.0");
+    app.setApplicationName(QStringLiteral("kquitapp"));
+    app.setApplicationVersion(QStringLiteral("2.0"));
 
     QCommandLineParser parser;
     parser.setApplicationDescription(QCoreApplication::translate("main", "Quit a D-Bus enabled application easily"));
-    parser.addOption(QCommandLineOption("service", QCoreApplication::translate("main", "Full service name, overrides application name provided"), "service"));
-    parser.addOption(QCommandLineOption("path", QCoreApplication::translate("main", "Path in the D-Bus interface to use"), "path", "/MainApplication"));
-    parser.addPositionalArgument("application", QCoreApplication::translate("main", "The name of the application to quit"));
+    parser.addOption(QCommandLineOption(QStringLiteral("service"), QCoreApplication::translate("main", "Full service name, overrides application name provided"), QStringLiteral("service")));
+    parser.addOption(QCommandLineOption(QStringLiteral("path"), QCoreApplication::translate("main", "Path in the D-Bus interface to use"), QStringLiteral("path"), QStringLiteral("/MainApplication")));
+    parser.addPositionalArgument(QStringLiteral("application"), QCoreApplication::translate("main", "The name of the application to quit"));
     parser.addHelpOption();
     parser.addVersionOption();
     parser.process(app);
@@ -39,7 +39,7 @@ int main(int argc, char* argv[])
     QString service;
     if (parser.isSet(QStringLiteral("service")))
     {
-        service = parser.value("service");
+        service = parser.value(QStringLiteral("service"));
     }
     else if(parser.positionalArguments().isEmpty())
     {
@@ -50,14 +50,14 @@ int main(int argc, char* argv[])
         parser.showHelp(1);
     }
 
-    QString path(parser.value("path"));
+    QString path(parser.value(QStringLiteral("path")));
 
     QDBusInterface interface(service, path);
     if (!interface.isValid()) {
         qWarning() << QCoreApplication::translate("main", "Application %1 could not be found using service %2 and path %3.").arg(parser.positionalArguments().first()).arg(service).arg(path);
         return 1;
     }
-    interface.call("quit");
+    interface.call(QStringLiteral("quit"));
     QDBusError error = interface.lastError();
     if (error.type() != QDBusError::NoError) {
         qWarning() << QCoreApplication::translate("main", "Quitting application %1 failed. Error reported was:\n\n     %2 : %3").arg(parser.positionalArguments().first()).arg(error.name()).arg(error.message());
