4cc09b0ac4dbdc1e2c0a8a1ca21de329f6bc6cfe
Andreas Cord-Landwehr <cordlandwehr@kde.org>
2021-03-27 09:30:57 +0100

    Relicense file to LGPL-2.0-or-later
    According to relicensecheck and manual check of authors, this change is
    confirmed, except:
    
    - lgplv2+: sergobot markuss reitelbach
     sergobot (   1 LOC): f798101
    
    This single-line krazy-fix is not copyrightable and thus do not need
    confirmation.

--- src/tools/kquitapp/kquitapp.cpp
+++ src/tools/kquitapp/kquitapp.cpp
@@ -1,7 +1,7 @@
 /*
     SPDX-FileCopyrightText: 2006 Aaron Seigo <aseigo@kde.org>
 
-    SPDX-License-Identifier: LGPL-2.0-only
+    SPDX-License-Identifier: LGPL-2.0-or-later
 */
 
 #include <QCommandLineParser>
