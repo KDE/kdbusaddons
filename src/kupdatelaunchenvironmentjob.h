ba8c48a604f09c7410f3ed41baf09d0430986f49
Thiago Sueto <herzenschein@gmail.com>
2025-03-10 21:04:47 +0000

    Port API documentation to QDoc syntax

--- src/kupdatelaunchenvironmentjob.h
+++ src/kupdatelaunchenvironmentjob.h
@@ -17,37 +17,54 @@
 class QString;
 class KUpdateLaunchEnvironmentJobPrivate;
 
-/**
- * @class KUpdateLaunchEnvironmentJob updatelaunchenvironmentjob.h <KUpdateLaunchEnvironmentJob>
- *
- * Job for updating the launch environment.
+/*!
+ * \class KUpdateLaunchEnvironmentJob
+ * \inmodule KDBusAddons
+ * \brief Job for updating the launch environment.
  *
  * This job adds or updates an environment variable in process environment that will be used
- * when a process is launched:
- * This includes:
- *  - DBus activation
- *  - Systemd units
- *  - Plasma-session
+ * when a process is launched. This includes:
+ *
+ * \list
+ * \li DBus activation
+ * \li Systemd units
+ * \li Plasma-session
+ * \endlist
  *
  * Environment variables are sanitized before uploading.
  *
- * This object deletes itself after completion, similar to KJobs
+ * This object deletes itself after completion, similar to KJobs.
+ *
+ * Example usage:
+ *
+ * \code
+ * QProcessEnvironment newEnv;
+ * newEnv.insert("VARIABLE"_s, "value"_s);
+ * auto job = new KUpdateLaunchEnvironmentJob(newEnv);
+ * QObject::connect(job, &KUpdateLaunchEnvironmentJob::finished, &SomeClass, &SomeClass::someSlot);
+ * \endcode
  *
  * Porting from KF5 to KF6:
  *
  * The class UpdateLaunchEnvironmentJob was renamed to KUpdateLaunchEnvironmentJob.
  *
- * @since 6.0
+ * \since 6.0
  */
 class KDBUSADDONS_EXPORT KUpdateLaunchEnvironmentJob : public QObject
 {
     Q_OBJECT
 
 public:
+    /*!
+     * Creates a new job for the given launch \a environment.
+     */
     explicit KUpdateLaunchEnvironmentJob(const QProcessEnvironment &environment);
     ~KUpdateLaunchEnvironmentJob() override;
 
 Q_SIGNALS:
+    /*!
+     * Emitted when the job is finished, before the object is automatically deleted.
+     */
     void finished();
 
 private:
