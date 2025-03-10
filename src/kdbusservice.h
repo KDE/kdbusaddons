0b3742a6e93aa4e9823fdfbc7cf329117a31582a
Thiago Sueto <herzenschein@gmail.com>
2025-03-10 21:04:47 +0000

    Port to QDoc
    Relevant metatask: https://invent.kde.org/teams/documentation/sprints/-/issues/17
    
    Instructions about the port to QDoc: https://invent.kde.org/teams/goals/streamlined-application-development-experience/-/issues/10

--- src/kdbusservice.h
+++ src/kdbusservice.h
@@ -18,43 +18,42 @@
 
 class KDBusServicePrivate;
 
-/**
- * @class KDBusService kdbusservice.h <KDBusService>
- *
- * KDBusService takes care of registering the current process with D-Bus.
+/*!
+ * \class KDBusService
+ * \inmodule KDBusAddons
+ * \brief KDBusService takes care of registering the current process with D-Bus.
  *
  * This registers the application at a predictable location on D-Bus, registers
  * the QCoreApplication (or subclass) object at /MainApplication, and
  * assists in implementing the application side of D-Bus activation from
- * the <a
- * href="http://standards.freedesktop.org/desktop-entry-spec/desktop-entry-spec-latest.html">Desktop
- * Entry Specification</a>.
+ * the \l {http://standards.freedesktop.org/desktop-entry-spec/desktop-entry-spec-latest.html}
+ * {Desktop Entry Specification}.
  *
  * An application can either work in Multiple mode or Unique mode.
  *
  * In Multiple mode, the application can be launched many times.  The service
  * name in the D-Bus registration will contain the PID to distinguish the
- * various instances; for example: <tt>org.kde.konqueror-12345</tt>.
+ * various instances; for example: \e org.kde.konqueror-12345.
  *
  * In Unique mode, only one instance of this application can ever run.
  * The first instance of the application registers with D-Bus without the PID,
  * and any attempt to run the application again will cause the
  * activateRequested() signal to be emitted in the already-running instance; the
  * duplicate instance will then quit. The exit value can be set by the already
- * running instance with setExitValue(), the default value is @c 0.
+ * running instance with setExitValue(), the default value is \c 0.
  *
  * Unique-mode applications should usually delay parsing command-line arguments
  * until after creating a KDBusService object; that way they know they are the
  * original instance of the application.
  *
- * Applications that set the D-Bus activation entry (DBusActivatable=true) in
+ * Applications that set the D-Bus activation entry (\c DBusActivatable=true) in
  * their desktop files will use Unique mode and connect to the signals emitted
  * by this class.
  * Note that the D-Bus interface is exported for Multiple-mode applications as
  * well, so it also makes sense for such applications to connect to the signals
  * emitted by this class.
  *
- * @note In order to avoid a race, the application should export its objects to
+ * \note In order to avoid a race, the application should export its objects to
  * D-Bus before allowing the event loop to run (for example, by calling
  * QCoreApplication::exec()).  Otherwise, the application will appear on the bus
  * before its objects are accessible via D-Bus, which could be a problem for
@@ -63,89 +62,77 @@ class KDBusServicePrivate;
  *
  * Example usage:
  *
- * @code
-     QApplication app(argc, argv);
-     app.setApplicationName("kuiserver");
-     app.setOrganizationDomain("kde.org");
-     // Create your D-Bus objects here
-     // ...
-     KDBusService service(KDBusService::Unique);
-     // If this point is reached, this is the only running instance
-     // i.e. org.kde.kuiserver has been registered
-     return app.exec();
- * @endcode
+ * \code
+ *   QApplication app(argc, argv);
+ *   app.setApplicationName("kuiserver");
+ *   app.setOrganizationDomain("kde.org");
+ *   // Create your D-Bus objects here
+ *   // ...
+ *   KDBusService service(KDBusService::Unique);
+ *   // If this point is reached, this is the only running instance
+ *   // i.e. org.kde.kuiserver has been registered
+ *   return app.exec();
+ * \endcode
  *
- * @since 5.0
+ * \since 5.0
  */
 class KDBUSADDONS_EXPORT KDBusService : public QObject
 {
     Q_OBJECT
 
 public:
-    /**
+    /*!
+     * \enum KDBusService::StartupOption
      * Options to control the behaviour of KDBusService
-     * @see StartupOptions
+     * \value Unique
+     * Indicates that only one instance of this application should ever exist.
+     * Cannot be combined with \c Multiple.
+     * \value Multiple
+     * Indicates that multiple instances of the application may exist.
+     * Cannot be combined with \c Unique.  This is the default.
+     * \value NoExitOnFailure
+     * Indicates that the application should not exit if it failed to
+     * register with D-Bus.
+     * If not set, KDBusService will quit the application if it failed to
+     * register the service with D-Bus or a \c Unique instance can not be
+     * activated. A \c Multiple instance will exit with error code \c 1.
+     * The exit value of a \c Unique instance can be set from the running
+     * instance with setExitValue(), the default value is \c 0.
+     * \value [since 5.65] Replace
+     * Indicates that if there's already a unique service running,
+     * to be quit and replaced with our own.
+     * If exported, it will try first quitting the service calling
+     * \c org.qtproject.Qt.QCoreApplication.quit,
+     * which is exported by KDBusService by default.
      */
     enum StartupOption {
-        /**
-         * Indicates that only one instance of this application should ever exist.
-         *
-         * Cannot be combined with @c Multiple.
-         */
         Unique = 1,
-        /**
-         * Indicates that multiple instances of the application may exist.
-         *
-         * Cannot be combined with @c Unique.  This is the default.
-         */
         Multiple = 2,
-        /** Indicates that the application should not exit if it failed to
-         * register with D-Bus.
-         *
-         * If not set, KDBusService will quit the application if it failed to
-         * register the service with D-Bus or a @c Unique instance can not be
-         * activated. A @c Multiple instance will exit with error code @c 1.
-         * The exit value of a @c Unique instance can be set from the running
-         * instance with setExitValue(), the default value is @c 0.
-         */
         NoExitOnFailure = 4,
-        /**
-         * Indicates that if there's already a unique service running, to be quit and replaced
-         * with our own.
-         *
-         * If exported, it will try first quitting the service calling @c org.qtproject.Qt.QCoreApplication.quit,
-         * which is exported by KDBusService by default.
-         *
-         * @since 5.65
-         */
         Replace = 8
     };
     Q_ENUM(StartupOption)
-
-    /**
-     * Stores a combination of #StartupOption values.
-     */
     Q_DECLARE_FLAGS(StartupOptions, StartupOption)
     Q_FLAG(StartupOptions)
 
-    /**
+    /*!
      * Tries to register the current process to D-Bus at an address based on the
-     * application name and organization domain.
+     * application name and organization domain under the given \a parent.
      *
      * The D-Bus service name is the reversed organization domain, followed by
-     * the application name.  If @p options includes the @c Multiple flag, the
-     * application PID will be appended.  For example,
-     * @code
+     * the application name.  If \a options includes the \c Multiple flag, the
+     * application PID will be appended.  For example:
+     * \code
      * app.setApplicationName("kuiserver");
      * app.setOrganizationDomain("kde.org");
-     * @endcode
-     * will make KDBusService register as @c org.kde.kuiserver in @c Unique
-     * mode, and @c org.kde.kuiserver-1234 (if the process has PID @c 1234) in
-     * @c Multiple mode.
+     * \endcode
+     * The above will make KDBusService register as \c org.kde.kuiserver in \c Unique
+     * mode, and \c org.kde.kuiserver-1234 (if the process has PID \c 1234) in
+     * \c Multiple mode.
      */
     explicit KDBusService(StartupOptions options = Multiple, QObject *parent = nullptr);
 
-    /**
+    /*!
      * Destroys this object (but does not unregister the application).
      *
      * Deleting this object before unregister() could confuse
@@ -154,7 +141,7 @@ public:
      */
     ~KDBusService() override;
 
-    /**
+    /*!
      * Returns true if the D-Bus registration succeeded.
      *
      * Note that this is only useful when specifying the option NoExitOnFailure.
@@ -163,14 +150,14 @@ public:
      */
     bool isRegistered() const;
 
-    /**
+    /*!
      * Returns the name of the D-Bus service registered by this class.
      * Mostly useful when using the option Multiple.
-     * @since 5.33
+     * \since 5.33
      */
     QString serviceName() const;
 
-    /**
+    /*!
      * Returns the error message from the D-Bus registration if it failed.
      *
      * Note that this is only useful when specifying the option NoExitOnFailure.
@@ -178,10 +165,10 @@ public:
      */
     QString errorMessage() const;
 
-    /**
+    /*!
      * Sets the exit value to be used for a duplicate instance.
      *
-     * If this is a @c Unique application, a slot connected to
+     * If this is a \c Unique application, a slot connected to
      * activateRequested() can use this to specify a non-zero exit value for the
      * duplicate instance.  This would typically be done if invalid command-line
      * arguments are passed.
@@ -189,15 +176,15 @@ public:
      * Note that this will only work if the signal-slot connection type is
      * Qt::DirectConnection.
      *
-     * @param value  The exit value for the duplicate instance.
+     * \a value  The exit value for the duplicate instance.
      */
     void setExitValue(int value);
 
 Q_SIGNALS:
-    /**
+    /*!
      * Signals that the application is to be activated.
      *
-     * If this is a @c Unique application, when KDBusService is constructed in
+     * If this is a \c Unique application, when KDBusService is constructed in
      * subsequent instances of the application (ie: when the executable is run
      * when an instance is already running), it will cause this signal to be
      * emitted in the already-running instance (with the arguments passed to the
@@ -205,22 +192,25 @@ Q_SIGNALS:
      *
      * If this application's desktop file indicates that it supports D-Bus
      * activation (DBusActivatable=true), a command launcher may also call the Activate()
-     * D-Bus method to trigger this signal. In this case, @p args will be empty.
+     * D-Bus method to trigger this signal. In this case, \a arguments will be empty.
      *
      * In single-window applications, the connected signal should typically
      * raise the window.
      *
-     * @param arguments  The arguments the executable was called with, starting with the executable file name.
-     *                   See QCoreApplication::arguments().
-     *                   This can also be empty.
+     * \a arguments  The arguments the executable was called with,
+     * starting with the executable file name.
+     * See QCoreApplication::arguments(). This can be empty.
+     *
+     * \a workingDirectory The directory from which the executable is called.
+     * This can be empty.
      *
      * A typical implementation of the signal handler would be:
-     * @code
-     *    if (!arguments.isEmpty()) {
-     *        commandLineParser->parse(arguments); // same QCommandLineParser instance as the one used in main()
-     *        handleCmdLine(workingDirectory); // shared method with main(), which uses commandLineParser to handle options and positional arguments
-     *    }
-     * @endcode
+     * \code
+     * if (!arguments.isEmpty()) {
+     *     commandLineParser->parse(arguments); // same QCommandLineParser instance as the one used in main()
+     *     handleCmdLine(workingDirectory); // shared method with main(), which uses commandLineParser to handle options and positional arguments
+     * }
+     * \endcode
      *
      * For GUI applications, the handler also needs to deal with any platform-specific startup ids
      * and make sure the mainwindow is shown as well as request its activation from the window manager.
@@ -228,24 +218,24 @@ Q_SIGNALS:
      * from QX11Info::nextStartupId(), if there is one.
      * For Wayland, KDBusService provides the token for the XDG Activation protocol in the
      * "XDG_ACTIVATION_TOKEN" environment variable and unsets it again after the signal, if there is one.
-     * The util method @c KWindowSystem::updateStartupId(QWindow *window) (since KF 5.91) takes care of that.
+     * The util method \c KWindowSystem::updateStartupId(QWindow *window) (since KF 5.91) takes care of that.
      * A typical implementation in the signal handler would be:
-     * @code
-     *    mainWindow->show();
-     *    KWindowSystem::updateStartupId(mainWindow->windowHandle());
-     *    mainWindow->raise();
-     *    KWindowSystem::activateWindow(mainWindow->windowHandle());
-     * @endcode
+     * \code
+     * mainWindow->show();
+     * KWindowSystem::updateStartupId(mainWindow->windowHandle());
+     * mainWindow->raise();
+     * KWindowSystem::activateWindow(mainWindow->windowHandle());
+     * \endcode
      *
-     * If you're using the builtin handling of @c --help and @c --version in QCommandLineParser,
+     * If you're using the builtin handling of \c --help and \c --version in QCommandLineParser,
      * you should call parser.process(arguments) before creating the KDBusService instance,
      * since parse() doesn't handle those (and exiting the already-running instance would be wrong anyway).
      *
-     * @see setExitValue()
+     * \sa setExitValue()
      */
     void activateRequested(const QStringList &arguments, const QString &workingDirectory);
 
-    /**
+    /*!
      * Signals that one or more files should be opened in the application.
      *
      * This signal is emitted to request handling of the respective method of the D-Bus activation.
@@ -254,12 +244,13 @@ Q_SIGNALS:
      * See documentation of activateRequested(const QStringList &arguments, const QString &)
      * for details.
      *
-     * @param uris  The URLs of the files to open.
+     * \a uris  The URLs of the files to open.
      */
     void openRequested(const QList<QUrl> &uris);
 
-    /**
-     * Signals that an application action should be triggered.
+    /*!
+     * Signals that an application action \a actionName should be triggered
+     * with the given \a parameter.
      *
      * This signal is emitted to request handling of the respective method of the D-Bus activation.
      * For GUI applications, the signal handler also needs to deal with any platform-specific startup ids
@@ -272,7 +263,7 @@ Q_SIGNALS:
     void activateActionRequested(const QString &actionName, const QVariant &parameter);
 
 public Q_SLOTS:
-    /**
+    /*!
      * Manually unregister the given serviceName from D-Bus.
      */
     void unregister();
--- src/kdedmodule.h
+++ src/kdedmodule.h
@@ -20,18 +20,16 @@ class Kded;
 class QDBusObjectPath;
 class QDBusMessage;
 
-/**
- * \class KDEDModule kdedmodule.h <KDEDModule>
- *
- * The base class for KDED modules.
+/*!
+ * \class KDEDModule
+ * \inmodule KDBusAddons
+ * \brief The base class for KDED modules.
  *
  * KDED modules are constructed as shared libraries that are loaded on-demand
  * into the kded daemon at runtime.
  *
  * See https://invent.kde.org/frameworks/kded/-/blob/master/docs/HOWTO
  * for documentation about writing kded modules.
- *
- * @author Waldo Bastian <bastian@kde.org>
  */
 class KDBUSADDONS_EXPORT KDEDModule : public QObject
 {
@@ -41,45 +39,48 @@ class KDBUSADDONS_EXPORT KDEDModule : public QObject
     friend class Kded;
 
 public:
-    /**
-     * Constructor
+    /*!
+     * \brief Creates a new kded module with the given \a parent.
      */
     explicit KDEDModule(QObject *parent = nullptr);
 
     ~KDEDModule() override;
 
-    /**
-     * Sets the name of the module, and uses it to register the module to D-Bus.
+    /*!
+     * Sets the \a name of the module, and uses it to register the module to D-Bus.
      *
      * For modules loaded as plugins by a daemon, this is called automatically
      * by the daemon after loading the module. Module authors should NOT call this.
      */
     void setModuleName(const QString &name);
 
+    /*!
+     * The name of the module used to register to D-Bus.
+     */
     QString moduleName() const;
 
-    /**
-     * Returns the module being called by this D-Bus message.
+    /*!
+     * Returns the module being called by this D-Bus \a message.
+     *
      * Useful for autoloading modules in kded and similar daemons.
-     * @since 5.7
+     * \since 5.7
      */
     static QString moduleForMessage(const QDBusMessage &message);
 
 Q_SIGNALS:
-    /**
-     * Emitted when a mainwindow registers itself.
+    /*!
+     * Emitted when a mainwindow with the specified \a windowId registers itself.
      */
     void windowRegistered(qlonglong windowId);
 
-    /**
-     * Emitted when a mainwindow unregisters itself.
+    /*!
+     * Emitted when a mainwindow with the specified \a windowId unregisters itself.
      */
     void windowUnregistered(qlonglong windowId);
 
-    /**
-     * Emitted after the module is registered successfully with D-Bus
-     *
-     * @since 4.2
+    /*!
+     * Emitted after the module is registered successfully with D-Bus under the specified D-Bus object \a path.
+     * \since 4.2
      */
     void moduleRegistered(const QDBusObjectPath &path);
 
